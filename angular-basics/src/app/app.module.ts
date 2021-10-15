fc13a49a24f1f70033e035cce9de3cfacc51b1ce
Miguel Campos <camposmiguel@gmail.com>
2021-10-15 12:15:37 +0200

    material all imports

--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './material-imports.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
