301caa05eb08652a012e242f6dccfb495697f8e7
Miguel Campos <camposmiguel@gmail.com>
2021-12-09 09:00:35 +0100

    sign in with google complete

--- angular-firebase-gasapp/src/app/components/login/login.component.ts
+++ angular-firebase-gasapp/src/app/components/login/login.component.ts
@@ -1,28 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import firebase from 'firebase/compat/app';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
+import firebase from 'firebase/compat/app';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/interfaces/user.interface';
+
+const COLLECTION_USERS = 'users'; 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userList!: Observable<User[]>;
 
-  constructor(public auth: AngularFireAuth) {
+  constructor(public auth: AngularFireAuth, private firestore: AngularFirestore) {
   }
 
   ngOnInit(): void {
+    this.getUserList();
   }
 
   login() {
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(resp => {
-      console.log(resp);
+      this.firestore.collection(COLLECTION_USERS).doc(resp?.user?.uid)
+      .set({ name: resp.user?.displayName, 
+        email: resp.user?.email, 
+        photo: resp.user?.photoURL });
+      localStorage.setItem('name', resp.user?.displayName? resp.user?.displayName: '');
+      localStorage.setItem('photo', resp.user?.photoURL? resp.user?.photoURL: '');
+      localStorage.setItem('uid', resp.user?.uid? resp.user?.uid: '');
     });
+
   }
 
   logout() {
     this.auth.signOut();
+  }
+
+  getUserList() {
+    this.userList = this.firestore.collection<User>(COLLECTION_USERS).valueChanges();
   }
 
 }
