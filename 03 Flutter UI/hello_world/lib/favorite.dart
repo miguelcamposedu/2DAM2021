4a1cc739cdf8757de37bf800ff5569ae9994215d
Miguel Campos <camposmiguel@gmail.com>
2022-01-11 09:03:13 +0100

    stateful

---
+++ 03 Flutter UI/hello_world/lib/favorite.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({ Key? key }) : super(key: key);
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
