7022c6f0b29f6eadf7681a9edea0f51f41ebc3c5
Miguel Campos <camposmiguel@gmail.com>
2022-01-25 11:36:01 +0100

    Minitwitter login

---
+++ 03 Flutter UI/flutter_post_request/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_post_request/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Minitwitter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
