49fa2ff45934f5c187dc9ea0d11f3a42b8a85257
Miguel Campos <camposmiguel@gmail.com>
2022-02-15 13:34:00 +0100

    bottom nav bar custom

--- 03 Flutter UI/flutter_miarmapp/lib/screens/menu_screen.dart
+++ 03 Flutter UI/flutter_miarmapp/lib/screens/menu_screen.dart
@@ -1,58 +1,62 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_miarmapp/screens/home_screen.dart';
 import 'package:flutter_miarmapp/screens/profile_screen.dart';
 import 'package:flutter_miarmapp/screens/search_screen.dart';
-import 'package:flutter_miarmapp/widgets/home_app_bar.dart';
 
 class MenuScreen extends StatefulWidget {
-  const MenuScreen({ Key? key }) : super(key: key);
+  const MenuScreen({Key? key}) : super(key: key);
 
   @override
   _MenuScreenState createState() => _MenuScreenState();
 }
 
 class _MenuScreenState extends State<MenuScreen> {
-  int _selectedIndex = 0;
+  int _currentIndex = 0;
 
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    HomeScreen(),
-    SearchScreen(),
-    ProfileScreen(),
+  List<Widget> pages = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const ProfileScreen()
   ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: CupertinoTabBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+        body: pages[_currentIndex], bottomNavigationBar: _buildBottomBar());
+  }
+
+  Widget _buildBottomBar() {
+    return Container(
+        decoration: BoxDecoration(
+            border: const Border(
+          top: BorderSide(
+            color: Color(0xfff1f1f1),
+            width: 1.0,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset('assets/images/avatar.jpeg'),
-            label: 'My profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
+        )),
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              child: Icon(Icons.home),
+              onTap: () {},
+            ),
+            GestureDetector(
+              child: Icon(Icons.search),
+              onTap: () {},
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.asset(
+                    'assets/images/avatar.jpeg',
+                    width: 30,
+                  )),
+            )
+          ],
+        ));
   }
 }
